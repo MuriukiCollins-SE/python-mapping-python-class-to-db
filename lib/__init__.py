0e71f5d517e54c8a486bf6d44851574601c94542
collins muriuki <muriukicollins48@gmail.com>
2025-05-25 17:20:44 +0300

    final commit

--- lib/__init__.py
+++ lib/__init__.py
@@ -3,3 +3,17 @@ import sqlite3
 
 CONN = sqlite3.connect('company.db')
 CURSOR = CONN.cursor()
+
+def init_db():
+    CURSOR.execute("""
+        CREATE TABLE IF NOT EXISTS departments (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            location TEXT
+        )
+    """)
+    CONN.commit()
+
+def drop_db():
+    CURSOR.execute("DROP TABLE IF EXISTS departments")
+    CONN.commit()
--- lib/department.py
+++ lib/department.py
@@ -1,4 +1,4 @@
-from __init__ import CURSOR, CONN
+from lib import CURSOR, CONN
 
 
 class Department:
@@ -10,3 +10,50 @@ class Department:
 
     def __repr__(self):
         return f"<Department {self.id}: {self.name}, {self.location}>"
+
+    @classmethod
+    def create_table(cls):
+        CURSOR.execute("""
+            CREATE TABLE IF NOT EXISTS departments (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                location TEXT
+            )
+        """)
+        CONN.commit()
+
+    @classmethod
+    def drop_table(cls):
+        CURSOR.execute("DROP TABLE IF EXISTS departments")
+        CONN.commit()
+
+    def save(self):
+        if self.id is None:
+            CURSOR.execute(
+                "INSERT INTO departments (name, location) VALUES (?, ?)",
+                (self.name, self.location)
+            )
+            self.id = CURSOR.lastrowid
+            CONN.commit()
+        else:
+            self.update()
+
+    @classmethod
+    def create(cls, name, location):
+        dept = cls(name, location)
+        dept.save()
+        return dept
+
+    def update(self):
+        CURSOR.execute(
+            "UPDATE departments SET name = ?, location = ? WHERE id = ?",
+            (self.name, self.location, self.id)
+        )
+        CONN.commit()
+
+    def delete(self):
+        CURSOR.execute(
+            "DELETE FROM departments WHERE id = ?",
+            (self.id,)
+        )
+        CONN.commit()
